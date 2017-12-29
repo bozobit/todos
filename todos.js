52d11a62a8627842fbcc4317fc0ed3c1baf13e3c
bozobit <rich.anderson@urgnt.com>
2017-12-29 17:39:43 +1300

    some fixes

--- todos.js
+++ todos.js
@@ -144,6 +144,48 @@ if(Meteor.isClient){
 
     });
 
+    Template.register.events({
+        'submit form': function(event){
+            event.preventDefault();
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            Accounts.createUser({
+                email: email,
+                password: password},
+                function(error){
+                    if(error){
+                        console.log(error.reason); // Output error if registration fails
+                    } else {
+                        Router.go("home"); // Redirect user if registration succeeds
+                    }
+                }
+            );
+        }
+    });
+
+    Template.login.events({
+        'submit form': function(event){
+            event.preventDefault();
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            Meteor.loginWithPassword(email, password, function(error){
+                if(error){
+                    console.log(error.reason);
+                } else {
+                    Router.go("home");
+                }
+            });
+        }
+    });
+
+    Template.navigation.events({
+        'click .logout': function(event){
+            event.preventDefault();
+            Meteor.logout();
+            Router.go('login');
+        }
+    });
+
 
 }  //END CLIENT CODE
 
